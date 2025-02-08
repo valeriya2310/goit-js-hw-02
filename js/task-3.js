6619df36c01950c675b0d1dff3d1e43a5969abfc
valeriya2310 <vp0732022011@gmail.com>
2025-02-08 20:36:57 +0200

    update index.html file

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,15 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content); 
-    const paddingWidth = parseFloat(padding) * 2; 
-    const borderWidth = parseFloat(border) * 2;
-    const totalWidth = contentWidth + paddingWidth + borderWidth;
-    return totalWidth;
+function checkForSpam(message){
+    const normalizeMessage = message.toLowerCase();
 
+    return normalizeMessage.includes("spam") || normalizeMessage.includes("sale");
+    
 }
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px")); 
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
